72354aa8b51ad6637df45b06cb8a37c21288debe
Isaac Wilhite <isaacwilhite987@gmail.com>
2023-09-17 16:21:21 -0600

    solution

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-// Code your solutions in this file
-function writeCards(namesArray, event){
-    let thankYouCards = []
-    for (let i = 0; i < namesArray.length; i++){
-        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+let namesArray = ["Guadalupe", "Ollie", "Aki"]
+let event = 'surprise'
+const writeCards = (namesArray, event) => {
+    let messagesArray = []
+    for (let i = 0; i < namesArray.length; i++) {
+        messagesArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
     }
-    return thankYouCards
+    return messagesArray
 }
-function countDown(startingNumber){
-    while (startingNumber > 0){
+const countDown = startingNumber => {
+    while (startingNumber > 0) {
+        console.log(startingNumber)
+        startingNumber -= 1 
+    }
     console.log(startingNumber)
-    startingNumber -= 1
 }
-console.log(startingNumber)
-}
-
+countDown(10)
